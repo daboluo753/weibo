eca612d9ef593a3a8314cd3eb77e7e00753468a4
Your Name <your@example.com>
2019-06-13 14:48:28 +0700

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,10 @@ class User extends Authenticatable
             $user->activation_token = Str::random(10);
         });
     }
+
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    } 
+    
 }
